b342b2741aafcd02b89d4b8806fa36d836bac1eb
DaniellWarrley <warlleydaniell1@gmail.com>
2025-11-19 23:57:53 -0300

    Update index.js

--- mensaPay/index.js
+++ mensaPay/index.js
@@ -12,8 +12,9 @@ app.use(cors())
 
 app.use('/api', routes)
 
+const PORT = process.env.PORT || 3000
 connectDb().then(() => {
-    app.listen(3000, () => {
+    app.listen(PORT, () => {
         console.log('Server is running on port 3000');
     })
     console.log('Connected to database')
